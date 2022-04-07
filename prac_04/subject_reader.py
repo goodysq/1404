cdcae1a989e769e04a7032620a2737ca909d34e3
ysq <yangshiqi1234@gmail.com>
2022-04-07 10:05:19 +0800

    Converting Data Strings to Lists and show data in a better form

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,22 +8,26 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    show_data(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        data.append(parts)
+    return data
     input_file.close()
+
+
+def show_data(data):
+    """"show the data in a better form"""
+    for line in data:
+        print(f"{line[0]} is taught by {line[1]:12} and has {line[2]:4} students")
 
 
 main()
